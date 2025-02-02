ea7edf1ad55f3a4e7bf00e076923e4f580137ba5
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2025-02-02 11:50:57 +0000

    src: Code improvements and fix S2vDbl256 error.
    I've now quickly proofread the code again after being notified of a mistake in S2vDbl256() (see #1). I incorrectly converted a backwards for loop to a forwards for loop when copying the reference implementation. I always proofread the code prior to publishing, so it's quite annoying I missed this.

--- src/XChaCha20SivDotNet/XChaCha20Siv.cs
+++ src/XChaCha20SivDotNet/XChaCha20Siv.cs
@@ -8,64 +8,64 @@ public static class XChaCha20Siv
     public const int KeySize = XChaCha20.KeySize;
     public const int NonceSize = 16;
     public const int TagSize = 32;
-    
+
     public static void Encrypt(Span<byte> ciphertext, ReadOnlySpan<byte> plaintext, ReadOnlySpan<byte> nonce, ReadOnlySpan<byte> key, ReadOnlySpan<byte> associatedData = default)
     {
         Validation.EqualToSize(nameof(ciphertext), ciphertext.Length, plaintext.Length + TagSize);
         Validation.EqualToSize(nameof(key), key.Length, KeySize);
-        
+
         Span<byte> subkeys = stackalloc byte[KeySize * 2], macKey = subkeys[..KeySize], encKey = subkeys[KeySize..];
-        BLAKE2b.ComputeTag(subkeys, Span<byte>.Empty, key);
-        
+        BLAKE2b.ComputeTag(subkeys, ReadOnlySpan<byte>.Empty, key);
+
         Span<byte> tag = ciphertext[^TagSize..];
         S2v(tag, plaintext, nonce, macKey, associatedData);
-        
+
         XChaCha20.Encrypt(ciphertext[..^TagSize], plaintext, tag[..XChaCha20.NonceSize], encKey);
         CryptographicOperations.ZeroMemory(subkeys);
     }
-    
+
     public static void Decrypt(Span<byte> plaintext, ReadOnlySpan<byte> ciphertext, ReadOnlySpan<byte> nonce, ReadOnlySpan<byte> key, ReadOnlySpan<byte> associatedData = default)
     {
         Validation.NotLessThanMin(nameof(ciphertext), ciphertext.Length, TagSize);
         Validation.EqualToSize(nameof(plaintext), plaintext.Length, ciphertext.Length - TagSize);
         Validation.EqualToSize(nameof(key), key.Length, KeySize);
-        
+
         Span<byte> subkeys = stackalloc byte[KeySize * 2], macKey = subkeys[..KeySize], encKey = subkeys[KeySize..];
-        BLAKE2b.ComputeTag(subkeys, Span<byte>.Empty, key);
-        
+        BLAKE2b.ComputeTag(subkeys, ReadOnlySpan<byte>.Empty, key);
+
         ReadOnlySpan<byte> tag = ciphertext[^TagSize..];
         XChaCha20.Decrypt(plaintext, ciphertext[..^TagSize], tag[..XChaCha20.NonceSize], encKey);
-        
+
         Span<byte> computedTag = stackalloc byte[TagSize];
         S2v(computedTag, plaintext, nonce, macKey, associatedData);
-        
+
         bool valid = ConstantTime.Equals(tag, computedTag);
         CryptographicOperations.ZeroMemory(subkeys);
         CryptographicOperations.ZeroMemory(computedTag);
-        
+
         if (!valid) {
             CryptographicOperations.ZeroMemory(plaintext);
             throw new CryptographicException();
         }
     }
-    
+
     private static void S2v(Span<byte> tag, ReadOnlySpan<byte> plaintext, ReadOnlySpan<byte> nonce, ReadOnlySpan<byte> macKey, ReadOnlySpan<byte> associatedData)
     {
         Span<byte> d = stackalloc byte[TagSize];
         BLAKE2b.ComputeTag(d, new byte[TagSize], macKey);
-        
+
         if (associatedData.Length > 0) {
             S2vDbl256(d);
             BLAKE2b.ComputeTag(tag, associatedData, macKey);
             S2vXor(d, tag, d.Length);
         }
-        
+
         if (nonce.Length > 0) {
             S2vDbl256(d);
             BLAKE2b.ComputeTag(tag, nonce, macKey);
             S2vXor(d, tag, d.Length);
         }
-        
+
         using var blake2b = new IncrementalBLAKE2b(TagSize, macKey);
         if (plaintext.Length >= TagSize) {
             blake2b.Update(plaintext[..^TagSize]);
@@ -79,7 +79,7 @@ public static class XChaCha20Siv
         blake2b.Update(d);
         blake2b.Finalize(tag);
     }
-    
+
     private static void S2vDbl256(Span<byte> d)
     {
         Span<byte> t = stackalloc byte[d.Length];
@@ -87,7 +87,7 @@ public static class XChaCha20Siv
         for (int i = 0; i < t.Length; i++) {
             t[i] = (byte)(t[i] << 1);
         }
-        for (int i = 0; i < t.Length - 2; i++) {
+        for (int i = 0; i < t.Length - 1; i++) {
             t[i] |= (byte)(d[i + 1] >> 7);
         }
         byte mask = (byte)~((d[0] >> 7) - 1);
@@ -96,7 +96,7 @@ public static class XChaCha20Siv
         t.CopyTo(d);
         CryptographicOperations.ZeroMemory(t);
     }
-    
+
     private static void S2vXor(Span<byte> d, ReadOnlySpan<byte> h, int length)
     {
         for (int i = 0; i < length; i++) {
